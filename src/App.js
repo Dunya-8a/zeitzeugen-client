dff3e68a56bc3d2f9c1ec3f77d4d55059cf36cfd
Dünya <53619378+Dunya-8a@users.noreply.github.com>
2022-04-03 22:56:38 +0100

    Add basic text to Landing page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 // IMPORT MODULES
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // IMPORT COMPONENTS AND STYLES
 import VideoPage from "./pages/VideoPage";
+import Home from "./pages/Home/Home";
+import Landing from "./pages/Landing/Landing";
+import Library from "./pages/Library/Library";
 
 const App = () => {
 	return (
 		<>
-			<VideoPage />
+			<Router>
+				<Switch>
+					<Route path="/" exact component={Landing} />
+					<Route path="/home" component={Home} />
+					<Route path="/library" component={Library} />
+					<Router path="/:videoId" component={VideoPage} />
+				</Switch>
+			</Router>
 		</>
 	);
 };
---
+++ src/pages/Landing/Landing.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Landing = () => {
+	return (
+		<>
+			<div>
+				<h1>Zeitzeugen</h1>
+				<p>Life is short and we never know when each of us will leave.</p>
+				<p>Our life stories are unique, but volatile.</p>
+				<p>In collective memory, however, we live eternally.</p>
+				<p>
+					With zeitzeugen, we want to collect the life stories of people of all
+					kinds, and store them for future generations.
+				</p>
+				<p>
+					Do you have a loved one, friend, mentor or acquaintance that you’d not
+					like to be forgotten? Who you’d like to live on in collective memory,
+					for your (grand-)children to meet and yourself to remember?
+				</p>
+				<p>
+					Then join our forces. Equipped with your smartphone, camera or voice
+					recorder, arrange a time to interview your chosen person. You can use
+					one of our question templates or come up with your own questions.
+				</p>
+				<p>
+					Upload the video to zeitzeugen. We will upload it to the
+					Interplanetary File System, a decentralized, censorship-resistant
+					store of data that can be accessed by anyone, forever.
+				</p>
+				<p>They shall not be forgotten.</p>
+			</div>
+		</>
+	);
+};
+
+export default Landing;
